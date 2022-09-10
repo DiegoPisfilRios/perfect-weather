cd90fc56637be90273d3f2d7845a15958e986b8f
Diego Pisfil Rios <fdiego.prios@gmail.com>
2022-09-10 17:24:38 -0500

    test

--- src/context/Weather.context.jsx
+++ src/context/Weather.context.jsx
@@ -6,8 +6,6 @@ const WeatherProvider = ({ children }) => {
     const [weather, setwather] = useState({});
     const [forecast, setforecast] = useState([]);
 
-    console.log(import.meta.env.VITE_APP_IPINFO);
-
     const URL_IPINFO = import.meta.env.VITE_APP_IPINFO;
     const TK_OPENWEATHER = import.meta.env.VITE_APP_TOKEN;
 
@@ -33,7 +31,7 @@ const WeatherProvider = ({ children }) => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?appid=${TK_OPENWEATHER}&lang=es&q=${city}&units=metric`)
             .then(result => result.json())
             .then(result => {
-                console.log(result);
+                console.info('RequestForCity: SUCCESS');
                 setFieldsWeather(result);
                 requestForecast({
                     lat: result.coord.lat,
@@ -47,7 +45,7 @@ const WeatherProvider = ({ children }) => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?appid=${TK_OPENWEATHER}&lang=es&lat=${latitude}&lon=${longitude}&units=metric`)
             .then(result => result.json())
             .then(result => {
-                console.log(result);
+                console.info('RequestForLocation: SUCCESS');
                 setFieldsWeather(result);
                 requestForecast({
                     lat: result.coord.lat,
@@ -61,7 +59,7 @@ const WeatherProvider = ({ children }) => {
         fetch(`http://api.openweathermap.org/data/3.0/onecall?lat=${coord.lat}&lon=${coord.lon}&lang=sp&units=metric&appid=${TK_OPENWEATHER}`)
             .then(result => result.json())
             .then(result => { 
-                console.log(result.daily);
+                console.info('RequestForecast: SUCCESS');
                 setforecast(result.daily)
             })
             .catch(err => console.error(err))
